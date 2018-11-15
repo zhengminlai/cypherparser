48831300d3420a3723dd574cf877461fec2fe507
karn <1906817459@qq.com>
2018-11-15 12:51:26 +0800

    inital commit

---
+++ src/main/scala/CypherToJson.scala
@@ -0,0 +1,73 @@
+import org.slizaa.neo4j.opencypher.openCypher.Cypher
+import org.slizaa.neo4j.opencypher.openCypher.impl._
+
+object CypherToJson{
+  def parseCypherToJson(cypher: Cypher) = {
+    var cypher_impl: CypherImpl = null
+    if (cypher.isInstanceOf[CypherImpl]){
+
+      cypher_impl = cypher.asInstanceOf[CypherImpl]
+      var statement: SinglePartQueryImpl = null
+
+      if (cypher_impl.getStatement().isInstanceOf[SinglePartQueryImpl]) {
+        statement = cypher_impl.getStatement().asInstanceOf[SinglePartQueryImpl]
+        val readingClauses = statement.getReadingClauses()
+        val readingClauseIter = readingClauses.iterator()
+
+        while (readingClauseIter.hasNext){
+          val readingClause = readingClauseIter.next()
+
+          if (readingClause.isInstanceOf[MatchImpl]){
+            val matchImpl = readingClause.asInstanceOf[MatchImpl]
+            val patterns = matchImpl.getPattern().getPatterns()
+            val patternsIter = patterns.iterator()
+
+            while (patternsIter.hasNext()) {
+              val p = patternsIter.next()
+
+              if (p.isInstanceOf[PatternElementImpl]) {
+                val pat = p.asInstanceOf[PatternElementImpl]
+                val node = pat.getNodepattern()
+                var nodeLabels: List[String] = List()
+                val nodeLabelIter = node.getNodeLabels().iterator()
+
+                while(nodeLabelIter.hasNext()){
+                  val labelStr = nodeLabelIter.next().getLabelName()
+                  nodeLabels = labelStr::nodeLabels
+                }
+
+                println("node name:" + node.getVariable().getName() + ", node labels: " + nodeLabels)
+                val elementChains = pat.getChain()
+                var lastNode = node
+                val chainIter = elementChains.iterator()
+
+                while (chainIter.hasNext()) {
+                  val chain = chainIter.next()
+
+                  if (chain.isInstanceOf[PatternElementChainImpl]) {
+                    val chainElem = chain.asInstanceOf[PatternElementChainImpl]
+                    val relationshipType = chainElem.getRelationshipPattern()
+                    val curNode = chainElem.getNodePattern()
+                    var curNodeLabels: List[String] = List()
+                    val curNodeLabelIter = curNode.getNodeLabels.iterator()
+
+                    while(curNodeLabelIter.hasNext()){
+                      val labelStr = curNodeLabelIter.next().getLabelName()
+                      curNodeLabels = labelStr::curNodeLabels
+                    }
+                    println("node name:" + curNode.getVariable().getName() + ", node labels: " + curNodeLabels)
+                    println("Relationship name: " + relationshipType.getDetail().getRelTypeNames() +
+                      ", incoming: " + relationshipType.isIncoming() + ", outgoing: " +
+                      relationshipType.isOutgoing() + ", between " + lastNode.getVariable().getName()
+                      + " and " + curNode.getVariable().getName())
+                    lastNode = curNode
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/CypherParserTest.scala
@@ -0,0 +1,28 @@
+
+import org.scalatest.FunSuite
+
+class CypherParserTest extends FunSuite {
+  test("infer schema for ConnectedSegments") {
+    try {
+      val cypher = CypherParser.parseString(
+        """MATCH
+          |  (sensor:Sensor)<-[mb1:monitoredBy]-(segment1:Segment),
+          |  (segment1:Segment)-[ct1:connectsTo]->
+          |  (segment2:Segment)-[ct2:connectsTo]->
+          |  (segment3:Segment)-[ct3:connectsTo]->
+          |  (segment4:Segment)-[ct4:connectsTo]->
+          |  (segment5:Segment)-[ct5:connectsTo]->(segment6:Segment),
+          |  (segment2:Segment)-[mb2:monitoredBy]->(sensor:Sensor),
+          |  (segment3:Segment)-[mb3:monitoredBy]->(sensor:Sensor),
+          |  (segment4:Segment)-[mb4:monitoredBy]->(sensor:Sensor),
+          |  (segment5:Segment)-[mb5:monitoredBy]->(sensor:Sensor),
+          |  (segment6:Segment)-[mb6:monitoredBy]->(sensor:Sensor)
+          |RETURN sensor, segment1, segment2, segment3, segment4, segment5, segment6
+          |""".stripMargin)
+      CypherToJson.parseCypherToJson(cypher)
+    } catch {
+      case e: Exception => println(e.toString())
+    }
+  }
+
+}
